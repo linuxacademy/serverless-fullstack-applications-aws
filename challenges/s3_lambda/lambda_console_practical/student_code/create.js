0d9eb9cf46b4eae008ce4a8d469a46e64e077a95
Fernando Medina Corey <fmcorey@gmail.com>
2017-10-25 10:36:01 -0700

    add create.js

---
+++ challenges/s3_lambda/lambda_console_practical/student_code/create.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+
+  if (typeof data.text !== 'string') {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t create the item.',
+    });
+    return;
+  }
+
+  const params = {
+    TableName: 'PrometheonServiceRecords',
+    Item: {
+      clientId: data.clientId,
+      timestamp: timestamp,
+      clientName: data.clientName,
+      serviceType: data.service,
+      serviceId: data.service + '_' + data.clientId + '_' + timestamp,
+      cost: data.cost,
+    },
+  };
+
+  // write the item to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the item.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+};
