5a20ab70cfb2b81a93a5b13f16286a9583e4d090
Fernando Medina Corey <fmcorey@gmail.com>
2017-10-25 14:13:29 -0700

    Add additional capability required to run

--- challenges/s3_lambda/lambda_console_practical/run.bash
+++ challenges/s3_lambda/lambda_console_practical/run.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws cloudformation create-stack --stack-name sls1-s3-lab1-lambda-console-practical --template-body file://mycf.json --capabilities CAPABILITY_IAM
+aws cloudformation create-stack --stack-name sls1-s3-lab1-lambda-console-practical --template-body file://mycf.json --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
