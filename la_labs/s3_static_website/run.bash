2be750f34dffada490fbad9105f367892f5b4f1a
Fernando Medina Corey <fmcorey@gmail.com>
2017-11-13 18:13:57 -0800

    Update run.bash to reflect correct lab in stack name

--- la_labs/s3_static_website/run.bash
+++ la_labs/s3_static_website/run.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws cloudformation create-stack --stack-name sls2-s4-lab1-api-gateway --template-body file://mycf.json --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+aws cloudformation create-stack --stack-name sls2-s5-lab1-static-site --template-body file://mycf.json --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
